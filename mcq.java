2dfe2f6365dc224f46df842437fca083ab884e4c
aric08 <aricmar26@crosspointacademy.org>
2025-09-27 02:59:29 +0000

    mcq

---
+++ mcq.java
@@ -0,0 +1,24 @@
+public class mcq {
+    public static void main(String[] args) {
+        // Q1
+        System.out.println("Q1: 10");
+
+        // Q2
+        System.out.println("Q2: 101");
+
+        // Q3
+        System.out.println("Q3: 10");
+
+        // Q4
+        System.out.println("Q4: Both B and D");
+
+        // Q5
+        System.out.println("Q5: Endless loop");
+
+        // Q6
+        System.out.println("Q6: 6 7");
+
+        // Q7
+        System.out.println("Q7: None of these (3040)");
+    }
+}
